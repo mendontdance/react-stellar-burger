06373f825ab758b38139f01b0e40ee292dd36786
Danil Khegai <danilmaster95@gmail.com>
2023-07-14 13:01:47 +0300

    14072023 - first commit

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,35 @@
+import React from 'react'
 import styles from "./app.module.css";
-import { data } from "../../utils/data";
+import AppHeader from "../appheader/AppHeader.jsx";
+import BurgerIngredients from '../burger-ingredients/BurgerIngredients'
+import BurgerConstructor from '../burger-constructor/BurgerConstructor'
+
 
 function App() {
-  return (
-    <div className={styles.app}>
-      <pre style={{
-      	margin: "auto",
-      	fontSize: "1.5rem"
-      }}>
-      	Измените src/components/app/app.jsx и сохраните для обновления.
-      </pre>
-    </div>
+
+  const [state, setState] = React.useState({ success: false });
+
+  React.useEffect(() => {
+    const url = 'https://norma.nomoreparties.space/api/ingredients';
+    const fetchData = async () => {
+      const result = await fetch(url)
+      result.json()
+        .then(res => {
+        setState(res);
+      })
+        .catch(err => console.log(err))
+    }
+    fetchData();
+  }, []);
+
+  return (state.success &&
+    <>
+      <AppHeader />
+      <main className={`ml-5 mr-5 mb-15 ${styles.main}`}>
+        <BurgerIngredients data={state.data} />
+        <BurgerConstructor data={state.data} />
+      </main>
+    </>
   );
 }
 
